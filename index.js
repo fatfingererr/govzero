e99063c376a26a71aeaf5ec805b73d41554788c8
RueiCiWang <fatfingererr@gmail.com>
2022-07-22 19:25:51 +0800

    Create a new token, governor on rinkeby

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var path = require("path")
 var express = require("express")
 var bodyParser = require("body-parser")
-var jsonParser = bodyParser.json()
 
 var app = express()
 var db = require("./db")
